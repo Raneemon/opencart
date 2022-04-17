a23515f98bd961b7db89621210bd002649978e28
Raneemon <raneem.khoulani.96@gmail.com>
2022-04-17 16:43:27 +0300

    first commit

---
+++ admin/language/en-gb/extension/theme/nice.php
@@ -0,0 +1,76 @@
+<?php
+
+// Heading
+$_['heading_title']                    = 'Nice theme';
+$_['text_heading_title']               = 'Nice theme';
+
+// Text
+$_['text_extension']                   = 'Extensions';
+$_['text_success']                     = 'Success: You have modified the default store theme!';
+$_['text_edit']                        = 'Edit Default Store Theme';
+$_['text_general']                     = 'General';
+$_['text_product']                     = 'Products';
+$_['text_image']                       = 'Images';
+
+$_['text_enabled']                       = 'Enabled';
+$_['text_disabled']                       = 'Disabled';
+
+$_['text_title']                       = 'Text';
+$_['text_link']                        = 'Link';
+$_['text_sort_order']                  = 'Sort Order';
+
+$_['text_banner']                  = 'Banner';
+
+
+
+// Tab Basic Settings
+$_['tab_basic_settings']               = 'Basic Settings';
+$_['entry_status']                     = 'Status';
+$_['entry_directory']                  = 'Theme Directory';
+$_['help_directory']                   = 'This field is only to enable older themes to be compatible with the new theme system. You can set the theme directory to use on the image size settings defined here.';
+
+$_['entry_product_limit']              = 'Default Items Per Page';
+$_['entry_product_description_length'] = 'List Description Limit';
+$_['entry_image_category']             = 'Category Image Size (W x H)';
+$_['entry_image_manufacturer']         = 'Manufacturer Image Size (W x H)';
+$_['entry_image_thumb']                = 'Product Image Thumb Size (W x H)';
+$_['entry_image_popup']                = 'Product Image Popup Size (W x H)';
+$_['entry_image_product']              = 'Product Image List Size (W x H)';
+$_['entry_image_additional']           = 'Additional Product Image Size (W x H)';
+$_['entry_image_related']              = 'Related Product Image Size (W x H)';
+$_['entry_image_compare']              = 'Compare Image Size (W x H)';
+$_['entry_image_wishlist']             = 'Wish List Image Size (W x H)';
+$_['entry_image_cart']                 = 'Cart Image Size (W x H)';
+$_['entry_image_location']             = 'Store Image Size (W x H)';
+$_['entry_width']                      = 'Width';
+$_['entry_height']                     = 'Height';
+
+// Help
+
+$_['help_product_limit']               = 'Determines how many catalog items are shown per page (products, categories, etc)';
+$_['help_product_description_length']  = 'In the list view, short description character limit (categories, special etc)';
+
+// Error
+$_['error_permission']                 = 'Warning: You do not have permission to modify the default store theme!';
+$_['error_limit']                      = 'Product Limit required!';
+$_['error_image_thumb']                = 'Product Image Thumb Size dimensions required!';
+$_['error_image_popup']                = 'Product Image Popup Size dimensions required!';
+$_['error_image_product']              = 'Product List Size dimensions required!';
+$_['error_image_category']             = 'Category List Size dimensions required!';
+$_['error_image_additional']           = 'Additional Product Image Size dimensions required!';
+$_['error_image_related']              = 'Related Product Image Size dimensions required!';
+$_['error_image_compare']              = 'Compare Image Size dimensions required!';
+$_['error_image_wishlist']             = 'Wish List Image Size dimensions required!';
+$_['error_image_cart']                 = 'Cart Image Size dimensions required!';
+$_['error_image_location']             = 'Store Image Size dimensions required!';
+
+
+// Tab Menu Top
+$_['tab_menu_top']               = 'Menu Top';
+$_['entry_menu_top_status']      = 'Menu Top Status';
+$_['text_menu_top_items']      = 'Menu Top Items';
+
+
+// Tab Footer
+$_['tab_footer']               = 'Tab Footer';
+$_['entry_footer_icons']       = 'Footer Icons';
---
+++ catalog/language/en-gb/extension/theme/nice.php
@@ -0,0 +1,11 @@
+<?php
+
+// Header
+$_['text_nice_account'] = 'Account';
+$_['text_menu_catalog'] = 'Catalog';
+
+
+
+// Product Page
+$_['tab_general'] = 'General Info';
+$_['error']['add_to_cart'] = 'Error! Please check the form below!';
